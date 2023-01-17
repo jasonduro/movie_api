268b2360073c3574a2912ea09e20be90db42b931
Jason <product@jasonlamanque.com>
2023-01-17 10:13:49 +0100

    added code from exercise 2.4

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 const express = require('express');
 const app = express();
 
-let myLogger = (req, res, next) => {
-    console.log(req.url);
-    next();
-  };
-  
-  app.use(myLogger);
-  
-  app.get('/', (req, res) => {
-    res.send('Welcome to my app!');
-  });
-  
-  app.get('/secreturl', (req, res) => {
-    res.send('This is a secret url with super top-secret content.');
-  });
-  
-  app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.');
-  });
-  
+let topBooks = [
+  {
+    title: 'Harry Potter and the Sorcerer\'s Stone',
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Lord of the Rings',
+    author: 'J.R.R. Tolkien'
+  },
+  {
+    title: 'Twilight',
+    author: 'Stephanie Meyer'
+  }
+];
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to my book club!');
+});
+
+app.get('/documentation', (req, res) => {                  
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/books', (req, res) => {
+  res.json(topBooks);
+});
+
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
+
