d934b81e51be96bc63651100994b11adb527340d
Jason <product@jasonlamanque.com>
2023-01-17 10:52:26 +0100

    added the request to send back repsonses
    including the timestamp of the request

--- index.js
+++ index.js
@@ -6,14 +6,24 @@ let myLogger = (req, res, next) => {
   next();
 };
 
+let requestTime = (req, res, next) => {
+  req.requestTime = Date.now();
+  next();
+};
+
 app.use(myLogger);
+app.use(requestTime);
 
 app.get('/', (req, res) => {
   res.send('Welcome to my app!');
+  responseText += '<small>Requested at: ' + req.requestTime + '</small>';
+  res.send(responseText);
 });
 
 app.get('/secreturl', (req, res) => {
   res.send('This is a secret url with super top-secret content.');
+  responseText += '<small>Requested at: ' + req.requestTime + '</small>';
+  res.send(responseText);
 });
 
 app.listen(8080, () => {
