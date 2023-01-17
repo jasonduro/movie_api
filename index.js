f40f40e15d4f6a07d31b2b41675b557f75d23733
Jason <product@jasonlamanque.com>
2023-01-17 11:58:17 +0100

    updated index.js with movie list

--- index.js
+++ index.js
@@ -4,20 +4,67 @@ const express = require('express');
   path = require('path');
   
 const app = express();
-  //create a write stream (in append mode)
-  //a 'log.txt' file is created in root directory 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log-text'), {
-  flags: 'a'})
+  
+//create a write stream (in append mode)
+ //a 'log.txt' file is created in root directory 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log-text'), {flags: 'a'})
 
 //setup the logger
-app.use(morgan('combined', {stream: accessLogStream}));
+app.use(morgan('common', {stream: accessLogStream}));
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my app!');
+//connects the static public folder
+app.use(express.static('public'));
+
+let myFlix = [
+  {
+    title: '2001: A Space Odyssey',
+    year: '1968'
+  },
+  {
+    title: 'The Terminator',
+    year: '1984'
+  },  
+  {
+    title: 'Planet of the Apes',
+    year: '1968'
+  },  
+  {
+    title: 'Alien',
+    year: '1979'
+  },  
+  {
+    title: 'Blade Runner',
+    year: '1982'
+  },  
+  {
+    title: 'Terminator 2: Judgement Day',
+    year: '1991'
+  },  
+  {
+    title: 'The Matrix',
+    year: '1999'
+  },  
+  {
+    title: 'Back to the Future',
+    year: '1985'
+  },  
+  {
+    title: 'Aliens',
+    year: '1986'
+  },  
+  {
+    title: 'Interstellar',
+    year: '2014'
+  }
+
+];
+
+app.get('/movies', (req, res) => {
+  res.json(myFlix);
 });
 
-app.get('/secreturl', (req, res) => {
-  res.send('This is a secret url with super top-secret content.');
+app.get('/', (req, res) => {
+  res.send('Welcome to MyFlix Movie App!');
 });
 
 //error handling in express
