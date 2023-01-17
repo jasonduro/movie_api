61a75fa55fa56741a932dcebc2d9cd44ea32d9ed
Jason <product@jasonlamanque.com>
2023-01-17 10:49:41 +0100

    added mylogger code from exercise

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const express = require('express');
 const app = express();
 
+let myLogger = (req, res, next) => {
+  console.log(req.url);
+  next();
+};
+
+app.use(myLogger);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my app!');
+});
+
+app.get('/secreturl', (req, res) => {
+  res.send('This is a secret url with super top-secret content.');
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
