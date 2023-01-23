2a28e695dc1aa5372c5ffd92f452afa1a0c83b5a
Jason <product@jasonlamanque.com>
2023-01-23 13:46:22 +0100

    updated server.js page

--- server.js
+++ server.js
@@ -183,16 +183,86 @@ let movies = [
     },
 ]
 
+//CREATE Function - Allow new users to register
+app.post('/users', (req, res) => {
+    const newUser = req.body;
 
-//Read Function - Return all movies
+    if (newUser.name) {
+        newUser.id = uuid.v4();
+        users.push(newUser);
+        res.status(201).json(newUser)
+    } else {
+        res.status(400).send('users need names')
+    }
+})
+
+//  UPDATE - Allow users to update their user info (username, password, email, date of birth)
+app.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const updatedUser = req.body;
+
+    let user = users.find( user => user.id == id );
+
+    if (user) {
+        user.name = updatedUser.name;
+        res.status(200).json(user);
+    } else {
+        res.status(400).send('no such user')
+    }
+})
+
+//  POST - Allow users to add a movie to their list of favorites
+app.post('/users/:id/:movieTitle', (req, res) => {
+    const { id, movieTitle } = req.params;
+
+    let user = users.find( user => user.id == id );
+
+    if (user) {
+        user.favoriteMovies.push(movieTitle);
+        res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
+    } else {
+        res.status(400).send('no such user')
+    }
+})
+
+//  DELETE - Allow users to remove a movie from their list of favorites
+app.delete('/users/:id/:movieTitle', (req, res) => {
+    const { id, movieTitle } = req.params;
+
+    let user = users.find( user => user.id == id );
+
+    if (user) {
+        user.favoriteMovies = user.favoriteMovies.filter( title => title !== movieTitle);
+        res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
+    } else {
+        res.status(400).send('no such user')
+    }
+})
+
+//  DELETE - Allow existing users to deregister
+app.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    let user = users.find( user => user.id == id );
+
+    if (user) {
+        users = users.filter( user => user.id != id);
+        res.status(200).send(`user ${id} has been deleted`);
+    } else {
+        res.status(400).send('no such user')
+    }
+})
+
+
+//READ Function - Return a list of ALL movies to the user
 app.get('/movies', (req, res) => {
     res.status(200).json(movies);
 })
 
-//Read Function - Return a specific movie with object destructuring
+//READ Function - Return data (description, genre, director, image URL, whether it’s featured or not) about a single movie by title - using object destructuring
 app.get('/movies/:title', (req, res) => {
     const { title } = req.params;
-    const movies = movies.find( moive => movies.Title === title );
+    const movie = movies.find( movie => movie.Title === title );
 
     if (movie) {
         res.status(200).json(movie);
@@ -201,5 +271,28 @@ app.get('/movies/:title', (req, res) => {
     }
 })
 
+//READ Function - Return the genre property of the movie object with dot syntax
+app.get('/movies/genre/:genreName', (req, res) => {
+    const { genreName } = req.params;
+    const genre = movies.find( movie => movie.Genre.Name === genreName ).Genre;
+
+    if (genre) {
+        res.status(200).json(genre);
+    } else {
+        res.status(400).send('no such genre')
+    }
+})
+
+//READ Function - Return Data about a Director by Name
+app.get('/movies/director/:directorName', (req, res) => {
+    const { directorName } = req.params;
+    const director = movies.find( movie => movie.Director.Name === directorName ).Director;
+
+    if (director) {
+        res.status(200).json(director);
+    } else {
+        res.status(400).send('no such director')
+    }
+})
 
 app.listen(8080, () => console.log("listening on 8080"))
