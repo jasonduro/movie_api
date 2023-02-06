6cb141640586fdff93cebcf88286e58a721eb7cd
Jason <product@jasonlamanque.com>
2023-02-06 10:57:06 +0100

    updated port listening

--- index.js
+++ index.js
@@ -221,8 +221,7 @@ require('./passport');
         });
     });
 
-    let port = process.env.PORT;
-    if (port == null || port == "") {
-      port = 8080;
-    }
-    app.listen(port);
+    let port = process.env.PORT || 8080;
+    app.listen(port, '0.0.0.0',() => {
+        console.log('Listening on Port ' + port);
+    });
