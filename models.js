82f8c8db60726a203165a8c422479c4736fe1df2
Jason <product@jasonlamanque.com>
2023-02-06 13:22:26 +0100

    updated models.js
    changed the strictquery in mongoose model to false

--- models.js
+++ models.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose'); 
+mongoose.set('strictQuery', false);
+mongoose.connect("mongodb://localhost:27017/cfDB", { useNewUrlParser: true });
 
 const bcrypt = require('bcrypt');
 
